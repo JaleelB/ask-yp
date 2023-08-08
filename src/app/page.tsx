69ded9c3ac1cc44f0fce4d7c400ce782dc70b7e1
Jaleel Bennett <jaleelbennett58@gmail.com>
2023-08-07 21:39:30 -0400

    feat: prompt dialog

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
 "use client"
+import PromptDialog from "@/components/prompt-dialog";
 import { Button } from "@/components/ui/button";
 import { Textarea } from "@/components/ui/textarea";
 import { cn } from "@/lib/utils";
+import * as React from "react";
+
 
 const exploreOptions = [
   {
@@ -35,91 +38,99 @@ const exploreOptions = [
 
 
 export default function Home() {
+
+  const [isDialogOpen, setIsDialogOpen] = React.useState(false)
+
   return (
     <>
-    <main className="container max-w-[1000px] display flex flex-col mx-auto flex-1">
-      <section className="w-full m-auto">
-        <div className="w-full flex flex-col gap-8">
-          <div className="max-w-[650px] mx-auto flex flex-col gap-4 items-center p-4">
-              <svg xmlns="http://www.w3.org/2000/svg" width="84" height="80" viewBox="0 0 84 80" fill="none">
-                <path d="M26.124 54.7311L29.4438 64.4561C29.5383 64.7333 29.7075 64.9821 29.9346 65.1776C30.1616 65.3731 30.4385 65.5084 30.7376 65.5701C31.0367 65.6319 31.3476 65.6178 31.6392 65.5294C31.9308 65.4409 32.193 65.2812 32.3995 65.0661L47.201 49.6895" stroke="white" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5"/>
-                <path d="M66.3005 41.7289C66.3005 41.7289 64.4455 51.0906 53.4747 51.0906C42.504 51.0906 35.5897 38.1973 27.3262 38.1973C17.5647 38.1973 15.001 46.0023 15.001 51.0906C15.001 55.4339 17.423 57.1989 20.629 57.1989C23.835 57.1989 29.82 52.0406 32.3855 49.3939H46.5605" stroke="white" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5"/>
-                <path d="M27.3262 38.1978C27.3262 33.7195 23.5498 30.2578 17.6365 30.2578C11.7233 30.2578 7.875 33.0411 7.875 40.9111C7.875 48.7811 15.0203 50.3411 15.0203 50.3411" stroke="white" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5"/>
-                <path d="M12.698 31.1689C10.4685 25.5656 15.9968 20.0806 24.5473 20.0806C37.5148 20.0806 38.689 40.1306 51.1228 40.1306C61.4548 40.1306 68.383 27.3689 63.6633 20.0139C57.393 10.2406 46.9088 12.0372 42.49 16.0406" stroke="white" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5"/>
-                <path d="M24.5472 20.0809C26.8992 12.5476 38.6907 12.3109 43.358 16.7559C48.0252 21.2009 47.775 27.5443 41.2265 34.4926M57.8252 38.1993C62.8442 43.9993 75.1817 43.1926 76.0725 31.6909C76.9632 20.1893 66.3897 20.3743 64.2775 21.1443" stroke="white" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5"/>
-              </svg>
-              <h1 className="text-4xl sm:text-5xl text-primary font-bold">Ask Yelp</h1>
-              <p className="max-w-[600px] mt-4 text-secondary text-center">AskYelp is an open source AI chatbot that can interact with the Yelp Fusion API with natural language using Open AI functions and the Vercel AI SDK</p>
-          </div>
-          <div className="hidden w-full md:flex flex-col lg:flex-row gap-4 md:gap-0 items-center px-4 md:px-0">
-            {
-              exploreOptions.map((option, index) => (
-                <div 
-                  key={index} 
-                  className="flex flex-col gap-4 md:items-center w-full sm:min-w-[300px] md:p-3 lg:p-6"
-                >
-                  <div className="flex lg:flex-col lg:items-center gap-4">
-                    {option.icon}
-                    <h2 className="lg:text-center text-lg text-primary font-bold">{option.title}</h2>
-                  </div>
-                  <p className="text-center text-secondary hidden lg:block">{option.description}</p>
-                </div> 
-              ))
-            }
-          </div>
-        </div>
-      </section>
-    </main>
-    <footer className="w-full">
-        <div className="mx-auto flex flex-col items-center justify-center">
-          <form className="w-full">
-            <div className="w-full relative px-4 py-[10px] max-w-[750px] mx-auto">
-              <Textarea
-                className={cn("h-[50px] items-center max-h-[200px] pt-3 resize-none")}
-                placeholder="Type your message here."
-              />
-              <Button 
-                className="absolute bg-transparent top-4 right-4 hover:bg-transparent"
-                size="sm"
-                variant="ghost"
-              >
-                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
-                  <path d="M2.72403 2.05294C2.63791 2.0098 2.54115 1.99246 2.4454 2.003C2.34965 2.01355 2.25899 2.05152 2.18431 2.11236C2.10963 2.17321 2.05412 2.25432 2.02444 2.34597C1.99476 2.43761 1.99219 2.53587 2.01703 2.62894L3.51503 8.24694C3.53978 8.33958 3.59065 8.42315 3.66157 8.48769C3.73249 8.55223 3.82048 8.59501 3.91503 8.61094L10.77 9.75294C11.049 9.79994 11.049 10.1999 10.77 10.2469L3.91603 11.3889C3.82129 11.4047 3.7331 11.4474 3.66198 11.5119C3.59087 11.5765 3.53986 11.6602 3.51503 11.7529L2.01703 17.3709C1.99219 17.464 1.99476 17.5623 2.02444 17.6539C2.05412 17.7456 2.10963 17.8267 2.18431 17.8875C2.25899 17.9484 2.34965 17.9863 2.4454 17.9969C2.54115 18.0074 2.63791 17.9901 2.72403 17.9469L17.724 10.4469C17.807 10.4054 17.8767 10.3415 17.9255 10.2626C17.9742 10.1837 18 10.0927 18 9.99994C18 9.90716 17.9742 9.81622 17.9255 9.73728C17.8767 9.65833 17.807 9.59451 17.724 9.55294L2.72403 2.05294Z" fill="white"/>
+      <main className="container max-w-[1000px] display flex flex-col mx-auto flex-1 pb-40">
+        <section className="w-full m-auto lg:m-8 lg:mx-auto 2xl:m-auto">
+          <div className="w-full flex flex-col gap-8">
+            <div className="max-w-[650px] mx-auto flex flex-col gap-4 items-center p-4">
+                <svg xmlns="http://www.w3.org/2000/svg" width="84" height="80" viewBox="0 0 84 80" fill="none">
+                  <path d="M26.124 54.7311L29.4438 64.4561C29.5383 64.7333 29.7075 64.9821 29.9346 65.1776C30.1616 65.3731 30.4385 65.5084 30.7376 65.5701C31.0367 65.6319 31.3476 65.6178 31.6392 65.5294C31.9308 65.4409 32.193 65.2812 32.3995 65.0661L47.201 49.6895" stroke="white" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5"/>
+                  <path d="M66.3005 41.7289C66.3005 41.7289 64.4455 51.0906 53.4747 51.0906C42.504 51.0906 35.5897 38.1973 27.3262 38.1973C17.5647 38.1973 15.001 46.0023 15.001 51.0906C15.001 55.4339 17.423 57.1989 20.629 57.1989C23.835 57.1989 29.82 52.0406 32.3855 49.3939H46.5605" stroke="white" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5"/>
+                  <path d="M27.3262 38.1978C27.3262 33.7195 23.5498 30.2578 17.6365 30.2578C11.7233 30.2578 7.875 33.0411 7.875 40.9111C7.875 48.7811 15.0203 50.3411 15.0203 50.3411" stroke="white" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5"/>
+                  <path d="M12.698 31.1689C10.4685 25.5656 15.9968 20.0806 24.5473 20.0806C37.5148 20.0806 38.689 40.1306 51.1228 40.1306C61.4548 40.1306 68.383 27.3689 63.6633 20.0139C57.393 10.2406 46.9088 12.0372 42.49 16.0406" stroke="white" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5"/>
+                  <path d="M24.5472 20.0809C26.8992 12.5476 38.6907 12.3109 43.358 16.7559C48.0252 21.2009 47.775 27.5443 41.2265 34.4926M57.8252 38.1993C62.8442 43.9993 75.1817 43.1926 76.0725 31.6909C76.9632 20.1893 66.3897 20.3743 64.2775 21.1443" stroke="white" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5"/>
                 </svg>
-              </Button>
+                <h1 className="text-4xl sm:text-5xl text-primary font-bold">Ask Yelp</h1>
+                <p className="max-w-[600px] mt-2 text-secondary text-center hidden md:block">AskYelp is an open source AI chatbot that can interact with the Yelp Fusion API with natural language using Open AI functions and the Vercel AI SDK</p>
             </div>
-          </form>
-          <div className="pb-3 pt-2 text-center text-xs px-10 md:px-[60px] md:pb-6 md:pt-3">
-            <p className="text-center text-xs text-primary/60">
-                This project waas built with{" "}
-                <a
-                    href="https://platform.openai.com/docs/guides/gpt/function-calling"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="transition-colors hover:text-primary-foreground"
-                >
-                    OpenAI Functions
-                </a>{" "}
-                and{" "}
-                <a
-                    href="https://sdk.vercel.ai/docs"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="transition-colors hover:text-primary-foreground"
-                >
-                    Vercel AI SDK
-                </a>
-                .{" "}
-                <a
-                    href="https://github.com/JaleelB/ask-yelp"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="transition-colors hover:text-primary-foreground"
-                >
-                    View the project repo here.
-                </a>
-            </p>
+            <div className="hidden w-full md:flex flex-col lg:flex-row gap-4 md:gap-0 items-center px-4 md:px-0">
+              {
+                exploreOptions.map((option, index) => (
+                  <div 
+                    key={index} 
+                    className="flex flex-col gap-4 md:items-center w-full sm:min-w-[300px] md:p-3 lg:p-6"
+                  >
+                    <div className="flex lg:flex-col lg:items-center gap-4">
+                      {option.icon}
+                      <h2 className="lg:text-center text-lg text-primary font-semibold">{option.title}</h2>
+                    </div>
+                    <p className="text-center text-secondary hidden lg:block">{option.description}</p>
+                  </div> 
+                ))
+              }
+            </div>
           </div>
+        </section>
+      </main>
+      <footer className="w-full fixed bottom-0 flex flex-col items-center space-y-2 p-5 pb-3 sm:px-0">
+        <div className="mx-auto flex w-full max-w-[750px] md:px-6 lg:px-0">
+          <div className="flex-grow"></div>
+          <PromptDialog
+            open={isDialogOpen}
+            onOpenChange={() => setIsDialogOpen(!isDialogOpen)}
+          />
+        </div>
+        <form className="w-full max-w-[750px] md:px-6 lg:px-0">
+          <div className="w-full relative py-[10px] mx-auto">
+            <Textarea
+              className={cn("h-[50px] bg-primary/[8%] items-center max-h-[200px] max pt-3 resize-none")}
+              placeholder="Type your message here."
+            />
+            <Button 
+              className="absolute bg-transparent top-4 right-1 hover:bg-transparent"
+              size="sm"
+              variant="ghost"
+            >
+              <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
+                <path d="M2.72403 2.05294C2.63791 2.0098 2.54115 1.99246 2.4454 2.003C2.34965 2.01355 2.25899 2.05152 2.18431 2.11236C2.10963 2.17321 2.05412 2.25432 2.02444 2.34597C1.99476 2.43761 1.99219 2.53587 2.01703 2.62894L3.51503 8.24694C3.53978 8.33958 3.59065 8.42315 3.66157 8.48769C3.73249 8.55223 3.82048 8.59501 3.91503 8.61094L10.77 9.75294C11.049 9.79994 11.049 10.1999 10.77 10.2469L3.91603 11.3889C3.82129 11.4047 3.7331 11.4474 3.66198 11.5119C3.59087 11.5765 3.53986 11.6602 3.51503 11.7529L2.01703 17.3709C1.99219 17.464 1.99476 17.5623 2.02444 17.6539C2.05412 17.7456 2.10963 17.8267 2.18431 17.8875C2.25899 17.9484 2.34965 17.9863 2.4454 17.9969C2.54115 18.0074 2.63791 17.9901 2.72403 17.9469L17.724 10.4469C17.807 10.4054 17.8767 10.3415 17.9255 10.2626C17.9742 10.1837 18 10.0927 18 9.99994C18 9.90716 17.9742 9.81622 17.9255 9.73728C17.8767 9.65833 17.807 9.59451 17.724 9.55294L2.72403 2.05294Z" fill="white"/>
+              </svg>
+            </Button>
+          </div>
+        </form>
+        <div className="pb-3 text-center text-xs px-4 md:px-[60px]">
+          <p className="text-center text-xs text-primary/60">
+              This project waas built with{" "}
+              <a
+                  href="https://platform.openai.com/docs/guides/gpt/function-calling"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="transition-colors hover:text-primary-foreground"
+              >
+                  OpenAI Functions
+              </a>{" "}
+              and{" "}
+              <a
+                  href="https://sdk.vercel.ai/docs"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="transition-colors hover:text-primary-foreground"
+              >
+                  Vercel AI SDK
+              </a>
+              .{" "}
+              <a
+                  href="https://github.com/JaleelB/ask-yelp"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="transition-colors hover:text-primary-foreground"
+              >
+                  View the project repo here.
+              </a>
+          </p>
         </div>
       </footer>
     </>
---
+++ src/components/prompt-dialog.tsx
@@ -0,0 +1,211 @@
+"use client"
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog"
+import { Button } from "@/components/ui/button"
+import { cn } from "@/lib/utils"
+import * as React from "react";
+
+
+type Prompt = {
+    category: string
+    titles: string[];
+    prompts: string[];
+}
+
+const prompts: Prompt[] = [
+    {
+      category: "Businesses",
+      titles: [
+        "Discover Nearby Businesses",
+        "Businesses Around You",
+        "Highly Rated Businesses",
+        "Local Business Directory",
+        "Businesses with Stellar Reviews"
+      ],
+      prompts: [
+        "Find a variety of businesses located near me",
+        "Search for businesses operating in my vicinity",
+        "Show me businesses near me with the highest ratings",
+        "Provide a list of local businesses in my area",
+        "Locate businesses near me that have received excellent reviews"
+      ]
+    },
+    {
+      category: "Restaurant/Food Services",
+      titles: [
+        "Dine Nearby",
+        "Culinary Delights Around You",
+        "Top Culinary Spots",
+        "Local Dining Options",
+        "Vegetarian/Vegan Friendly Eateries"
+      ],
+      prompts: [
+        "Discover restaurants and eateries located close to me",
+        "Search for restaurants offering unique dishes in my area",
+        "Show me restaurants near me that are highly recommended by locals",
+        "Provide a list of local restaurants suitable for a family dinner",
+        "Locate restaurants near me that offer vegetarian and vegan options"
+      ]
+    },
+    {
+      category: "Local Services",
+      titles: [
+        "Services at Your Doorstep",
+        "Local Service Providers",
+        "Top Service Recommendations",
+        "Directory of Local Services",
+        "Trusted Local Services"
+      ],
+      prompts: [
+        "Find local services that can assist with my needs",
+        "Search for service providers operating in my vicinity",
+        "Show me local services that come highly recommended",
+        "Provide a directory of services available in my area",
+        "Locate local services near me with a track record of reliability"
+      ]
+    },
+    {
+      category: "Entertainment",
+      titles: [
+        "Entertainment Hotspots",
+        "Nightlife Adventures",
+        "Top Bars and Clubs",
+        "Local Entertainment Guide",
+        "Upcoming Local Events"
+      ],
+      prompts: [
+        "Discover entertainment options for a fun-filled evening near me",
+        "Search for nightlife spots to enjoy with friends in my area",
+        "Show me bars and clubs near me that are popular on weekends",
+        "Provide a guide to local entertainment and cultural events",
+        "Locate events, concerts, or shows happening near me this week"
+      ]
+    },
+    {
+      category: "Health & Wellness",
+      titles: [
+        "Wellness Centers Nearby",
+        "Fitness and Gym Options",
+        "Top Yoga and Meditation Studios",
+        "Holistic Wellness Centers",
+        "Health and Wellness Workshops"
+      ],
+      prompts: [
+        "Find health and wellness centers near me for holistic care",
+        "Search for gyms and fitness centers in my area with modern equipment",
+        "Show me yoga studios near me that offer both beginner and advanced classes",
+        "Provide a list of wellness centers that focus on holistic health",
+        "Locate workshops or seminars near me focused on health and well-being"
+      ]
+    },
+    {
+      category: "Shopping & Retail",
+      titles: [
+        "Shopping Destinations",
+        "Retail Therapy Spots",
+        "Fashion and Clothing Stores",
+        "Local Artisanal Shops",
+        "Bookstores and Reading Spaces"
+      ],
+      prompts: [
+        "Discover shopping centers near me for a day of retail therapy",
+        "Search for retail stores in my area offering seasonal discounts",
+        "Show me clothing stores near me that offer both casual and formal wear",
+        "Find local artisanal shops or boutiques with unique products",
+        "Locate bookstores near me with a cozy reading space"
+      ]
+    }
+]
+
+
+  
+export default function PromptDialog({
+    open,
+    onOpenChange
+}:{
+    open: boolean
+    onOpenChange: () => void
+}){
+
+    const [selectedPromptCategory, setSelectedPromptCategory] = React.useState<string>("Businesses");
+    const [selectedPromptIndex, setSelectedPromptIndex] = React.useState<number>(0);
+
+    return (
+        <Dialog open={open} onOpenChange={onOpenChange}>
+            <DialogTrigger asChild>
+                <Button 
+                    variant="outline"
+                    className={cn("text-primary text-xs border border-primary/[8%] bg-neutral-800 hover:bg-neutral-700 hover:text-primary")}
+                    onClick={() => onOpenChange()}
+                >
+                    Open Dialog
+                </Button>
+            </DialogTrigger>
+            <DialogContent className={cn("max-w-[1000px] flex flex-col h-screen md:h-auto")}>
+                <DialogHeader>
+                    <DialogTitle>Quick Prompts</DialogTitle>
+                    <DialogDescription>Select from these prompts to quickly get started.</DialogDescription>
+                </DialogHeader>
+                <div className="flex flex-col md:flex-row w-full items-start justify-start border-t border-t-primary/[8%] overflow-auto">
+                    <div className="flex flex-col gap-1 md:w-3.5/12 self-stretch overflow-scroll border-b border-b-primary/[8%] md:border-b-0 md:border-r md:border-r-primary/[8%] pb-3 md:pb-0 pt-3 pr-3 max-h-[500px]">
+                        {
+                            prompts.map((prompt, index) => (
+                                <div 
+                                    className={cn(`p-3 items-center justify-start font-normal text-base text-primary-foreground hover:bg-primary/[8%] hover:text-primary ${prompt.category === selectedPromptCategory && "bg-primary/[8%] text-primary"}`)}
+                                    key={index * Math.random()}
+                                    onClick={() => setSelectedPromptCategory(prompt.category)}
+                                >
+                                   {prompt.category}
+                                </div>
+                            ))
+                        }
+                    </div>
+                    <div className="flex flex-col gap-1 md:w-4/12 self-stretch overflow-y-auto border-b border-b-primary/[8%] md:border-b-0 md:border-r md:border-r-primary/[8%] p-3 pl-0 md:pl-3 md:pb-3 max-h-[500px]">
+                        {
+                            prompts.map((prompt) => (
+                                selectedPromptCategory === prompt.category &&
+                                (
+                                    prompt.titles.map((title, index) => (
+                                        <div 
+                                            className={cn(`p-3 items-center justify-start font-normal text-base text-primary-foreground hover:bg-primary/[5%] hover:text-primary ${index === selectedPromptIndex && "bg-primary/[8%] text-primary"}`)}
+                                            key={index * Math.random()}
+                                            onClick={() => setSelectedPromptIndex(index)}
+                                        >
+                                            {title}
+                                        </div>
+                                    ))
+                                )
+                            ))
+                        }
+                    </div>
+                    <div className="flex md:w-5/12 flex-col items-end self-stretch pr-0  max-h-[500px] p-3 pb-6 pl-0 md:pl-3 md:pb-3">
+                        <div className="relative h-full w-full overflow-y-scroll whitespace-pre-wrap rounded-md bg-primary/[5%] p-3 text-primary-foreground">
+                            <span className="sticky top-0 z-50 flex w-full bg-grey-100 pb-2 text-xs text-grey-400">PREVIEW (WORKSPACE PROMPT)</span>
+                            <div className="mt-4">
+                                {
+                                    prompts.map((prompt, index) => (
+                                        selectedPromptCategory === prompt.category &&
+                                        (
+                                            <span key={index * Math.random()}>{prompt.prompts[selectedPromptIndex]}</span>
+                                        )
+                                    ))
+                                }
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="flex">
+                    <div className="flex-grow"></div>
+                    <Button className="bg-primary/[8%] text-primary border border-primary/[8%] hover:bg-primary/[10%]">Use Prompt</Button>
+                </div>
+                
+            </DialogContent>
+        </Dialog>
+    )
+}
