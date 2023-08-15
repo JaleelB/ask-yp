94f8f3482a5f0b27f21c59f5d0f9ea516ff12990
Jaleel Bennett <jaleelbennett58@gmail.com>
2023-08-14 20:44:21 -0400

    chore: env variables config

--- src/env.mjs
+++ src/env.mjs
@@ -8,6 +8,11 @@ export const env = createEnv({
    */
   server: {
     // DATABASE_URL: z.string().url(),
+    OPENAI_API_KEY: z.string().min(1),
+    YELP_CLIENT_ID: z.string().min(1),
+    YELP_API_KEY: z.string().min(1),
+    YELP_API_ENDPOINT: z.string().url(),
+    IPINFO_TOKEN: z.string().min(1),
     NODE_ENV: z.enum(["development", "test", "production"]),
   },
 
@@ -26,7 +31,12 @@ export const env = createEnv({
    */
   runtimeEnv: {
     // DATABASE_URL: process.env.DATABASE_URL,
+    OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+    YELP_CLIENT_ID: process.env.YELP_CLIENT_ID,
+    YELP_API_KEY: process.env.YELP_API_KEY,
+    YELP_API_ENDPOINT: process.env.YELP_API_ENDPOINT,
     NODE_ENV: process.env.NODE_ENV,
+    IPINFO_TOKEN: process.env.IPINFO_TOKEN,
     // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
   },
   /**
